856bddd400de955506420f2ee8138b3e1de154a8
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-03-02 15:48:42 +0330

    fix: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,13 @@ import type { AppProps } from 'next/app'
 
 // ########## Libraries and Components ##########
 import Head from 'next/head'
+import router from 'next/router'
+import nProgress from 'nprogress'
+import '@/styles/nprogress.scss'
+
+router.events.on('routeChangeStart', nProgress.start)
+router.events.on('routeChangeError', nProgress.done)
+router.events.on('routeChangeComplete', nProgress.done)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
