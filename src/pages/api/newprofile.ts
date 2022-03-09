43681e4d4522b18b15f97442cf2e65be9456b311
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-03-09 16:16:15 +0330

    fix: add new profile data to database

--- src/pages/api/newprofile.ts
+++ src/pages/api/newprofile.ts
@@ -1,28 +1,42 @@
 import { execFileSync } from 'child_process'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { openDB } from '@/db'
+import type { OVPNProfile } from '@/lib/types'
 
 interface ResponseData {
   output: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   console.log('req.body:', req.body)
+  const reqData = req.body as OVPNProfile
 
   // const output = execFileSync('./src/scripts/createNewProfile.sh') //TODO: test this on the server
   try {
     const output = execFileSync(
       './src/scripts/createNewProfile.sh',
-      [req.body.name, req.body.startDate, req.body.subscriptionLength],
+      [reqData.name],
       { encoding: 'utf-8' }
+    )
+    const db = await openDB()
+    const sql =
+      'INSERT INTO Profile (name, startDate, subscriptionLength) VALUES (?, ?, ?)'
+    await db.run(
+      sql,
+      reqData.name,
+      reqData.startDate,
+      reqData.subscriptionLength
     )
 
     console.log('createNewProfile.sh OUTPUT:', output)
     res.status(200).json({ output })
   } catch (err) {
     console.error('createNewProfile.sh ERROR OCCURRED:', err)
-    res.status(500).json({ output: 'createNewProfile.sh ERROR OCCURRED' })
+    res
+      .status(500)
+      .json({ output: 'createNewProfile.sh ERROR OCCURRED: \n' + String(err) })
   }
 }
