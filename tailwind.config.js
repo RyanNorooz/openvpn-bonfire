358494679aeb8b1aa17dd87002f11cfc271f6f70
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-04-24 20:14:15 +0430

    fix: add firefox modifier for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   // darkMode: 'media', // 'media' or 'class' or false
@@ -13,7 +14,23 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     // require('tailwindcss-logical'),
-    // ...
+
+    // firefox only modifier
+    plugin(function ({ addVariant, e, postcss }) {
+      addVariant('firefox', ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: '-moz-document',
+          params: 'url-prefix()',
+        })
+        isFirefoxRule.append(container.nodes)
+        container.append(isFirefoxRule)
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`
+        })
+      })
+    }),
   ],
 
   theme: {
