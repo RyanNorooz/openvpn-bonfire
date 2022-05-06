6402ea9ddf733406ccbabd0365436b6b33bc1ef7
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-05-06 12:20:01 +0430

    fix: updating scripts based on rename happened on service name

--- scripts/functions.py
+++ scripts/functions.py
@@ -4,7 +4,7 @@ def add_dir_and_get_data():
     item = "WorkingDirectory"
     pwd = os.getcwd()
 
-    file = open("openvpn-automation.service", "r")
+    file = open("openvpn-bonfire.service", "r")
     lines = file.readlines()
     file.close()
 
@@ -17,12 +17,12 @@ def add_dir_and_get_data():
     return lines_str
 
 def write_data(data):
-    file = open("/etc/systemd/system/openvpn-automation.service", "w")
+    file = open("/etc/systemd/system/openvpn-bonfire.service", "w")
     file.write(data)
     file.close()
 
 def is_service_defined():
-    res = subprocess.run(['systemctl', 'is-enabled', 'openvpn-automation.service'], stdout=subprocess.PIPE)
+    res = subprocess.run(['systemctl', 'is-enabled', 'openvpn-bonfire.service'], stdout=subprocess.PIPE)
     output = res.stdout.decode('utf-8').strip()
     if output != "enabled":
         return False
--- scripts/setup.py
+++ scripts/setup.py
@@ -8,16 +8,16 @@ if sys.argv[1] == "install":
         print("===Activating Service===")
         write_data(add_dir_and_get_data())
         os.system("sudo systemctl daemon-reload")
-        os.system("sudo systemctl enable openvpn-automation.service")
+        os.system("sudo systemctl enable openvpn-bonfire.service")
 
 elif sys.argv[1] == "restart":
     print("===Restart Service===")
-    os.system("sudo systemctl restart openvpn-automation.service")
+    os.system("sudo systemctl restart openvpn-bonfire.service")
     print("===It Should Be Done===")
 
 elif sys.argv[1] == "remove":
     print("===Removing Service===")
-    os.system("sudo systemctl stop openvpn-automation.service")
-    os.system("sudo systemctl disable openvpn-automation.service")
-    os.system("sudo rm /etc/systemd/system/openvpn-automation.service")
+    os.system("sudo systemctl stop openvpn-bonfire.service")
+    os.system("sudo systemctl disable openvpn-bonfire.service")
+    os.system("sudo rm /etc/systemd/system/openvpn-bonfire.service")
     os.system("sudo systemctl daemon-reload")
